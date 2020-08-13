2dafa6e648fc56222cf994b51de1755992a873be
Wilfred Lopez <cubamc@gmail.com>
2020-08-13 13:30:09 -0400

    Added Image Field to Recipe Model : Please enter the commit message for your changes. Lines starting

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,15 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser,
                                         BaseUserManager, PermissionsMixin)
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -52,3 +60,18 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255, blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
@@ -25,3 +25,21 @@ class TagViewSet(BaseRecipeAttr):
 class IngredientViewSet(BaseRecipeAttr):
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecepeViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'retrive':
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
+
+    def perform_create(self, serial):
+        serial.save(user=self.request.user)
